95f6aeff27beb2e5d0c63b489da75b72064fceff
Anthony Ricardo <147872743+anthony-ricardox@users.noreply.github.com>
2026-08-30 18:12:31 -0300

    criado tabela com autor e books

--- Associations/db/migrate/20260830205752_create_books.rb
+++ Associations/db/migrate/20260830205752_create_books.rb
@@ -2,6 +2,7 @@ class CreateBooks < ActiveRecord::Migration[8.1]
   def change
     create_table :books do |t|
       t.string :name
+      t.references :author, null: false, foreign_key: true
 
       t.timestamps
     end
